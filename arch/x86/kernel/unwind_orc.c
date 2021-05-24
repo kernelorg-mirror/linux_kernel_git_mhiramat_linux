ac3db223446c1d5a62f3fa3bc09b4763062fd869
Masami Hiramatsu <mhiramat@kernel.org>
2021-05-24 11:43:49 +0900

    x86/unwind: Recover kretprobe trampoline entry
    Since the kretprobe replaces the function return address with
    the kretprobe_trampoline on the stack, x86 unwinders can not
    continue the stack unwinding at that point, or record
    kretprobe_trampoline instead of correct return address.
    
    To fix this issue, find the correct return address from task's
    kretprobe_instances as like as function-graph tracer does.
    
    With this fix, the unwinder can correctly unwind the stack
    from kretprobe event on x86, as below.
    
               <...>-135     [003] ...1     6.722338: r_full_proxy_read_0: (vfs_read+0xab/0x1a0 <- full_proxy_read)
               <...>-135     [003] ...1     6.722377: <stack trace>
     => kretprobe_trace_func+0x209/0x2f0
     => kretprobe_dispatcher+0x4a/0x70
     => __kretprobe_trampoline_handler+0xca/0x150
     => trampoline_handler+0x44/0x70
     => kretprobe_trampoline+0x2a/0x50
     => vfs_read+0xab/0x1a0
     => ksys_read+0x5f/0xe0
     => do_syscall_64+0x33/0x40
     => entry_SYSCALL_64_after_hwframe+0x44/0xae
    
    
    Reported-by: Daniel Xu <dxu@dxuuu.xyz>
    Signed-off-by: Masami Hiramatsu <mhiramat@kernel.org>
    Suggested-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Tested-by: Andrii Nakryik <andrii@kernel.org>
    ---
      Changes in v7:
       - Remove superfluous #include <linux/kprobes.h>.
      Changes in v5:
       - Fix the case of interrupt happens on kretprobe_trampoline+0.
      Changes in v3:
       - Split out the kretprobe side patch
       - Fix build error when CONFIG_KRETPROBES=n.
      Changes in v2:
       - Remove kretprobe wrapper functions from unwind_orc.c
       - Do not fixup state->ip when unwinding with regs because
         kretprobe fixup instruction pointer before calling handler.

--- arch/x86/kernel/unwind_orc.c
+++ arch/x86/kernel/unwind_orc.c
@@ -534,9 +534,8 @@ bool unwind_next_frame(struct unwind_state *state)
 		if (!deref_stack_reg(state, ip_p, &state->ip))
 			goto err;
 
-		state->ip = ftrace_graph_ret_addr(state->task, &state->graph_idx,
-						  state->ip, (void *)ip_p);
-
+		state->ip = unwind_recover_ret_addr(state, state->ip,
+						    (unsigned long *)ip_p);
 		state->sp = sp;
 		state->regs = NULL;
 		state->prev_regs = NULL;
@@ -549,7 +548,15 @@ bool unwind_next_frame(struct unwind_state *state)
 					 (void *)orig_ip);
 			goto err;
 		}
-
+		/*
+		 * There is a small chance to interrupt at the entry of
+		 * kretprobe_trampoline where the ORC info doesn't exist.
+		 * That point is right after the RET to kretprobe_trampoline
+		 * which was modified return address. So the @addr_p must
+		 * be right before the regs->sp.
+		 */
+		state->ip = unwind_recover_kretprobe(state, state->ip,
+				(unsigned long *)(state->sp - sizeof(long)));
 		state->regs = (struct pt_regs *)sp;
 		state->prev_regs = NULL;
 		state->full_regs = true;
@@ -562,6 +569,9 @@ bool unwind_next_frame(struct unwind_state *state)
 					 (void *)orig_ip);
 			goto err;
 		}
+		/* See UNWIND_HINT_TYPE_REGS case comment. */
+		state->ip = unwind_recover_kretprobe(state, state->ip,
+				(unsigned long *)(state->sp - sizeof(long)));
 
 		if (state->full_regs)
 			state->prev_regs = state->regs;
